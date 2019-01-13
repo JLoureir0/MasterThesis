775066a656426e858912bad9cad77a54c2af6e0e
João Loureiro <joao.a.g.loureiro@gmail.com>
2019-01-13 01:19:03 +0000

    Initial version of the stats database that contains statistical measurements of the data of all other databases's attributes and a new html page to display that information

--- webapp/stats/urls.py
+++ webapp/stats/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('', views.home_stats, name='stats-home'),
     path('<database>/', views.database_stats, name='stats-database'),
     path('<database>/<collection>', views.collection_stats, name='stats-collection'),
+    path('<database>/<collection>/<attribute>', views.attribute_stats, name='stats-attribute'),
     path('about/', views.about, name='stats-about')
 ]
--- webapp/stats/views.py
+++ webapp/stats/views.py
@@ -19,7 +19,7 @@ def database_stats(request, database):
     
     context['database'] = database
 
-    records = mongo_client['stats']['records']
+    records = mongo_client['stats']['database']
     context['collections'] = sorted(records.find_one({ 'database': database })['collections'].items())
 
     return render(request, 'stats/database.html', context)
@@ -30,11 +30,20 @@ def collection_stats(request, database, collection):
     
     context['database'] = database
     context['collection'] = collection
-    stat = mongo_client['stats']['attributes'].find_one({ 'database': database, 'collection': collection })
+    stat = mongo_client['stats']['collection'].find_one({ 'database': database, 'collection': collection })
     context['attributes'] = stat['attributes']
     
     return render(request, 'stats/collection.html', context)
 
+def attribute_stats(request, database, collection, attribute):
+    if database not in mongo_client.list_database_names() or collection not in mongo_client[database].list_collection_names():
+        raise Http404
+    if attribute not in mongo_client[database][collection].find_one({}):
+        raise Http404
+
+    context['attribute_stats'] = mongo_client['stats']['statistics'].find_one({ 'database': database, 'collection': collection, 'attribute': attribute })
+
+    return render(request, 'stats/attribute.html', context)
 
 def about(request):
     return render(request, 'stats/about.html', context)
